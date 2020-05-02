d986fbcd22588af4385c4c239465cad323fbdac5
Nikita Yankov <nickrocket97@gmail.com>
2020-05-02 15:11:50 +0300

    Initial Commit

---
+++ SwiftUI Lesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Lesson
+//
+//  Created by Никита Янков on 02.05.2020.
+//  Copyright © 2020 Никита Янков. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
